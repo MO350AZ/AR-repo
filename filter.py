b11a243b162d4cba8a85af9dc90a59a0e183e7e7
MO350AZ <96011129+MO350AZ@users.noreply.github.com>
2026-02-04 19:46:56 +0200

    Update filter.py

--- filter.py
+++ filter.py
@@ -6,19 +6,13 @@ SOURCE_URL = "https://raw.githubusercontent.com/keiyoushi/extensions/repo/index.
 with urllib.request.urlopen(SOURCE_URL, timeout=30) as response:
     data = json.loads(response.read().decode("utf-8"))
 
-# data هنا List مش Dict
-filtered_packages = []
+# data = list
+filtered = []
 
 for pkg in data:
-    # شيل الإباحي فقط
+    # استبعاد الإباحي فقط
     if not pkg.get("nsfw", False):
-        filtered_packages.append(pkg)
-
-filtered = {
-    "name": "AR Safe Extensions (Mirror of Keiyoushi)",
-    "source": SOURCE_URL,
-    "packages": filtered_packages
-}
+        filtered.append(pkg)
 
 with open("index.min.json", "w", encoding="utf-8") as f:
     json.dump(filtered, f, ensure_ascii=False, separators=(",", ":"))
